9a3b4bf8b97875334bf996e160ea5a54fd9041fc
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-06-20 10:13:31 +0200

    More abstraction.

--- resources/dev/common.nix
+++ resources/dev/common.nix
@@ -1,49 +1,11 @@
-{ pkgs, phpPkg }:
+{ pkgs, phpPkg, phpIni, phpExtensions }:
 with pkgs;
 let
     php = phpPkg;
 
-    options = ''
-            max_execution_time = 0
-            xdebug.mode=debug
-            memory_limit=-1
-    '';
-
     phpOverride = php.buildEnv {
-      extensions = { all, ... }: with all; [
-        # Mandatory
-        filter
-        iconv
-        ctype
-        redis
-        tokenizer
-        simplexml
-
-        # Recommendations
-        dom
-        posix
-        intl
-        opcache
-
-        # Optional
-        pcov
-        pdo_sqlite
-        pdo_mysql
-        pdo_pgsql
-
-        openssl
-        calendar
-        soap
-        mbstring
-        exif
-        fileinfo
-        gd
-        curl
-        zip
-        xmlwriter
-        xdebug
-      ];
-      extraConfig = options;
+      extensions = phpExtensions;
+      extraConfig = phpIni;
     };
 
 in mkShell {
--- resources/dev/php74/default.nix
+++ resources/dev/php74/default.nix
@@ -1,2 +1,47 @@
 { pkgs ? (import <nixpkgs> {}), ... }:
-(import ../common.nix) { inherit pkgs; phpPkg = pkgs.php74;}
+
+(import ../common.nix) {
+    inherit pkgs;
+
+    phpPkg = pkgs.php74;
+
+    phpIni = ''
+        max_execution_time = 0
+        xdebug.mode=debug
+        memory_limit=-1
+    '';
+
+    phpExtensions = { all, ... }: with all; [
+        # Mandatory
+        filter
+        iconv
+        ctype
+        redis
+        tokenizer
+        simplexml
+
+        # Recommendations
+        dom
+        posix
+        intl
+        opcache
+
+        # Optional
+        pcov
+        pdo_sqlite
+        pdo_mysql
+        pdo_pgsql
+
+        openssl
+        calendar
+        soap
+        mbstring
+        exif
+        fileinfo
+        gd
+        curl
+        zip
+        xmlwriter
+        xdebug
+    ];
+}
