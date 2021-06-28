5ec6f37059923bfcaed02d8aba5d2cbf10bf2298
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-06-28 22:24:24 +0200

    Update dependencies.

--- resources/dev/common.nix
+++ resources/dev/common.nix
@@ -1,9 +1,46 @@
 {
   pkgs ? (import <nixpkgs> {}),
   nix-phps ? import (fetchTarball https://github.com/fossar/nix-phps/archive/57fda5bb79afb05d0cac2c2f1025339250beb81c.tar.gz),
-  version,
-  phpIni,
-  phpExtensions
+  version ? "php74",
+  phpIni ? ''
+    max_execution_time = 0
+    xdebug.mode=debug
+    memory_limit=-1
+    '',
+  phpExtensions ? { all, ... }: with all; [
+        # Mandatory
+        filter
+        iconv
+        ctype
+        redis
+        tokenizer
+        simplexml
+
+        # Recommendations
+        dom
+        posix
+        intl
+        opcache
+
+        # Optional
+        calendar
+        curl
+        exif
+        fileinfo
+        gd
+        mbstring
+        openssl
+        pcov
+        pdo_sqlite
+        pdo_mysql
+        pdo_pgsql
+        soap
+        xdebug
+        xmlreader
+        xmlwriter
+        zip
+        zlib
+    ]
 }:
 
 let
