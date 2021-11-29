3a5aae330a4894503492791614901c5f3f52f4a8
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-11-29 07:40:16 +0100

    Update flake.

--- flake.nix
+++ flake.nix
@@ -17,70 +17,70 @@
       in {
         devShells = {
           default = import ./resources/dev/common.nix {
-            pkgs = pkgs;
+            inherit pkgs;
             phps = phps.packages.${system};
             version = "php80";
             phpExtensions = default: { all, ... }: builtins.filter (x: !builtins.elem x []) (default all);
           };
 
           php56 = import ./resources/dev/common.nix {
-            pkgs = pkgs;
+            inherit pkgs;
             phps = phps.packages.${system};
             version = "php56";
             phpExtensions = default: { all, ... }: builtins.filter (x: !builtins.elem x []) (default all);
           };
 
           php70 = import ./resources/dev/common.nix {
-            pkgs = pkgs;
+            inherit pkgs;
             phps = phps.packages.${system};
             version = "php70";
             phpExtensions = default: { all, ... }: builtins.filter (x: !builtins.elem x []) (default all);
           };
 
           php71 = import ./resources/dev/common.nix {
-            pkgs = pkgs;
+            inherit pkgs;
             phps = phps.packages.${system};
             version = "php71";
             phpExtensions = default: { all, ... }: builtins.filter (x: !builtins.elem x []) (default all);
           };
 
           php72 = import ./resources/dev/common.nix {
-            pkgs = pkgs;
+            inherit pkgs;
             phps = phps.packages.${system};
             version = "php72";
             phpExtensions = default: { all, ... }: builtins.filter (x: !builtins.elem x []) (default all);
           };
 
           php73 = import ./resources/dev/common.nix {
-            pkgs = pkgs;
+            inherit pkgs;
             phps = phps.packages.${system};
             version = "php73";
             phpExtensions = default: { all, ... }: builtins.filter (x: !builtins.elem x []) (default all);
           };
 
           php74 = import ./resources/dev/common.nix {
-            pkgs = pkgs;
+            inherit pkgs;
             phps = phps.packages.${system};
             version = "php74";
             phpExtensions = default: { all, ... }: builtins.filter (x: !builtins.elem x []) (default all);
           };
 
           php74-nodebug = import ./resources/dev/common.nix {
-            pkgs = pkgs;
+            inherit pkgs;
             phps = phps.packages.${system};
             version = "php74";
             phpExtensions = default: { all, ... }: builtins.filter (x: !builtins.elem x [all.xdebug all.pcov]) (default all);
           };
 
           php80 = import ./resources/dev/common.nix {
-            pkgs = pkgs;
+            inherit pkgs;
             phps = phps.packages.${system};
             version = "php80";
             phpExtensions = default: { all, ... }: builtins.filter (x: !builtins.elem x []) (default all);
           };
 
           php80-nodebug = import ./resources/dev/common.nix {
-            pkgs = pkgs;
+            inherit pkgs;
             phps = phps.packages.${system};
             version = "php80";
             phpExtensions = default: { all, ... }: builtins.filter (x: !builtins.elem x [all.xdebug all.pcov]) (default all);
