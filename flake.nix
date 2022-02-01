d8ba66217dcb4c4f6d0f34432a4b8746af6493cb
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-02-01 22:49:55 +0100

    Add quotes.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "PHP/Composer/SymfonyCLi/GithubCLi/git/sqlite/make/docker-compose";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     phps.url = "github:fossar/nix-phps";
   };
