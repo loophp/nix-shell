43a2af3b6bc3eb3dcb029010fed55a1593884d75
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-07-14 08:59:00 +0200

    refactor: switch to `nix-systems/default` for handling  default systems

--- flake.nix
+++ flake.nix
@@ -4,20 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-phps.url = "github:fossar/nix-phps";
-  };
+    systems.url = "github:nix-systems/default";
+    };
 
   outputs = inputs @ {
     self,
+    systems,
     flake-parts,
     ...
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
+      systems = import systems;
 
       imports = [
         ./src/composer.nix
