b54d47bb05020592c996570b7a1f11b63b841cae
Pol Dellaiera <pol.dellaiera@protonmail.com>
2026-09-21 18:09:38 +0200

    chore: fix url to project

--- flake.nix
+++ flake.nix
@@ -1,4 +1,4 @@
-# DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
+# DO-NOT-EDIT. This file was auto-generated using github:denful/flake-file.
 # Use `nix run .#write-flake` to regenerate it.
 {
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
